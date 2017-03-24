e19628daaf2df6507011d218506c670a8756049d
markus <markus2b@yahoo.com>
2017-03-24 00:56:40 -0400

    Files
    Files

---
+++ Assets/Scripts/ValueFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using Valve.VR.InteractionSystem;
+
+
+public class ValueFollow : MonoBehaviour {
+
+	public LinearMapping linearMapping;
+	public Slider slider;
+
+
+	void Update () {
+		slider.value = linearMapping.value;
+	}
+}
---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+	float speed = 2.0f;
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+		transform.position += transform.forward * speed * Time.deltaTime;
+
+	}
+}
